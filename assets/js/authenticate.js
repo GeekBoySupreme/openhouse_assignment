cf85096ace29e6d533b33f7929ce9272051fe253
geekboysupreme <shuvam.manna@gmail.com>
2020-01-06 20:25:10 +0530

    updates

--- assets/js/authenticate.js
+++ assets/js/authenticate.js
@@ -1,8 +1,17 @@
 function authenticate()
 {
-    var phone_number=document.getElementById("exampleInputPhone");
-    var password=document.getElementById("exampleInputPassword1");
+    var phone_number=document.getElementById("exampleInputPhone").value;
+    var password=document.getElementById("exampleInputPassword1").value;
+    console.log(phone_number);
 
-    
+    if ( phone_number == "000000" && password == "helloworld"){
+        //alert("Successful");
+        window.location = "onboard.html"; // Redirecting to other page.
+        return false;
+        }
+        else{
+            //document.getElementById("validate_message").style.display="block";
+            document.getElementById("validate_message").innerHTML="Wrong Inputs! Try again."
+        }
     
 }
