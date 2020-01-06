f2a6fd20b4db915e9c401e2d78906cc6e352f4e9
geekboysupreme <shuvam.manna@gmail.com>
2020-01-07 00:13:34 +0530

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -104,3 +104,29 @@ function gettime(time, day, subject)
     }
     return code;
 }
+
+
+
+function addToConfirmation()
+{
+    html='';
+    console.log("Function Hit");
+    /*var pill_id = subject+"/"+day;
+    var pill = document.getElementById(pill_id);
+
+    var pill_class = subject+"/"+day;
+    var pill_cluster = document.getElementsByClassName(pill_class);
+
+    for (i = 0; i < pill_cluster.length; i++) {
+        pill_cluster[i].className = pill_cluster[i].className.replace(" active", "");
+      }
+
+    pill.style.background="#fc6666";
+    pill.className += "active";
+
+
+    html='<div id='+ subject +'><h4 class="description">'+ subject +'</h4><h5 class="description>'+ day +', <span id="blue_text>'+ time +'</span></h5></div>';
+    $('#schedule_container').append(html);
+
+    document.getElementById("schedule_container").innerHTML += html;*/
+}
